4112ab804a418297e7ca3dbffa3d0d318a8b578e
Balraj Duhre <Bduhre-cc@conestogac.on.ca>
2020-09-29 21:04:53 -0400

    name added

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ import aItems from "../items/index.js";
 
 class Page {
     constructor() {
-        this.sName = "Balraj Duhre";
+        this.sName = "Balraj Duhre !!!!!!!!!!!!!!!";
         const sBase = document.location.pathname;
         if (sBase[sBase.length - 1] == "/") {
             this.sBase = sBase.substr(0, sBase.length - 1);
